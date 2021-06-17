5518cd6ca0dfc067a14e54d5e58914d7dffb2cef
Stoyann <stoyann.openclassrooms@gmail.com>
2021-06-17 19:17:03 +1100

    function afficher info photographerPage

--- scripts/photographer_pages.js
+++ scripts/photographer_pages.js
@@ -10,44 +10,29 @@ let currentPhotographer = new PhotographerList();
 fetch(linkToData)
   .then((reponse) => reponse.json())
   .then((data) => {
-    getCurrentPhoptographer(data);
-    displayBanner();
+    // getCurrentPhoptographer(data);
+    displayBanner(data);
   });
 
-function getCurrentPhoptographer(data) {
+function displayBanner(data) {
   data.photographers.forEach((photographer) => {
     if (photographer.id === Number(urlParams.get("id"))) {
-      currentPhotographer.addPhotographer(
-        new Photographers(
-          photographer.name,
-          photographer.id,
-          photographer.city,
-          photographer.country,
-          photographer.tags,
-          photographer.tagline,
-          photographer.price,
-          photographer.portrait
-        )
-      );
-      console.log(photographer);
+      // ajoute le nom du photographe sur le titre de la page
+      document.title += " - " + photographer.name;
+      // création de la banniere
+      const banerBody = document.createElement("div");
+      const banerTitle = document.createElement("h1");
+      const banerLocation = document.createElement("p");
+      const banerTagline = document.createElement("p");
+      banerTitle.textContent = photographer.name;
+      banerLocation.textContent =
+        photographer.city + " ," + photographer.country;
+
+      banerTagline.textContent = photographer.tagline;
+      banner.appendChild(banerBody);
+      banerBody.appendChild(banerTitle);
+      banerBody.appendChild(banerLocation);
+      banerBody.appendChild(banerTagline);
     }
   });
-}
-
-function displayBanner() {
-  // ajoute le nom du photographe sur le titre de la page
-  document.title += " - " + photographer.name;
-  // création de la banniere
-  const banerBody = document.createElement("div");
-  const banerTitle = document.createElement("h1");
-  const banerLocation = document.createElement("p");
-  const banerTagline = document.createElement("p");
-  banerTitle.textContent = photographer.name;
-  banerLocation.textContent = photographer.city + " ," + photographer.country;
-
-  banerTagline.textContent = photographer.tagline;
-  banner.appendChild(banerBody);
-  banerBody.appendChild(banerTitle);
-  banerBody.appendChild(banerLocation);
-  banerBody.appendChild(banerTagline);
 }
