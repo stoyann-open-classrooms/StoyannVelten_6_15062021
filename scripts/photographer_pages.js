ef87f0ae11378beb22f30ac54d49e9a536db2ad3
Stoyann <stoyann.openclassrooms@gmail.com>
2021-06-18 18:13:18 +1100

    modal input

--- scripts/photographer_pages.js
+++ scripts/photographer_pages.js
@@ -33,10 +33,19 @@ function displayBanner(data) {
       const tagsSpan = document.createElement("span");
       const containerImgBanner = document.createElement("div");
       const bannerImg = document.createElement("img");
-      const bannerModal = document.createElement("form");
+      const bannerModal = document.createElement("div");
+      const bannerModalHeader = document.createElement;
       const modalClose = document.createElement("p");
       const modalTitle = document.createElement("h3");
-
+      const bannerForm = document.createElement("form");
+      const formFirstName = document.createElement("label");
+      const formFirstNameInp = document.createElement("input");
+      const formLastName = document.createElement("label");
+      const formLastNameInp = document.createElement("input");
+      const formEmail = document.createElement("label");
+      const formEmailInp = document.createElement("input");
+      const formTxt = document.createElement("label");
+      const formTxtInp = document.createElement("textarea");
       banerTitle.textContent = photographer.name;
       banerLocation.textContent =
         photographer.city + " ," + photographer.country;
@@ -49,6 +58,10 @@ function displayBanner(data) {
       banner.append(banerBody, containerBtnBaner, containerImgBanner);
       modalTitle.textContent = `Contactez-Moi ${photographer.name}`;
       modalClose.textContent = "X";
+      formFirstName.innerHTML = "Prénom";
+      formLastName.innerHTML = "Nom";
+      formEmail.innerHTML = "Email<br>";
+      formTxt.innerHTML = "Votre Message<br>";
 
       banerBody.append(
         banerTitle,
@@ -63,6 +76,17 @@ function displayBanner(data) {
       containerImgBanner.appendChild(bannerImg);
       banerBody.append(bannerModal);
       bannerModal.append(modalTitle, modalClose);
+      bannerModal.append(bannerForm);
+      bannerForm.appendChild(formFirstName);
+      bannerForm.appendChild(formFirstNameInp);
+      bannerForm.appendChild(formLastName);
+
+      bannerForm.appendChild(formLastNameInp);
+      bannerForm.appendChild(formEmailInp);
+      bannerForm.appendChild(formEmail);
+      bannerForm.appendChild(formTxt);
+      bannerForm.appendChild(formTxtInp);
+      // bannerForm.append(formLastName, formLastNameInp);
 
       //  ouvrir la modal de la bannieres
       btnBaner.addEventListener("click", () => {
