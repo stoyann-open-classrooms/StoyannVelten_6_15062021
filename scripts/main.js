2b7d1ce8a182ef82771a052a1b20f0345948ac86
Stoyann <stoyann.openclassrooms@gmail.com>
2021-06-21 13:42:18 +1100

    css page acceuil

--- scripts/main.js
+++ scripts/main.js
@@ -49,6 +49,8 @@ function displayTags() {
     // a.setAttribute("aria-labelledby", `${tag}`);
 
     span.id = `${tag}`;
+    span.classList.add("tags");
+    a.classList.add("tags-link");
 
     tagList.append(a);
     a.append(span);
@@ -80,13 +82,24 @@ function displayPhotographers() {
     const cardLink = document.createElement("a");
     const cardImg = document.createElement("img");
     const cardTitle = document.createElement("h2");
+    const cardBody = document.createElement("div");
     const cardLocation = document.createElement("p");
     const cardTagline = document.createElement("p");
     const cardPrice = document.createElement("p");
     const cardTags = document.createElement("div");
-    const tagsA = document.createElement("a");
-    const tagsspan = document.createElement("span");
+
     const modalBg = document.createElement("form");
+
+    photographer.tags.forEach((el) => {
+      const tagsA = document.createElement("a");
+      const tagsspan = document.createElement("span");
+      cardTags.classList.add("cards-tags");
+      tagsspan.textContent = photographer.tags;
+      cardTags.append(tagsA);
+      tagsA.append(tagsspan);
+      tagsspan.textContent = "#" + el;
+      tagsA.classList.add("tags-link");
+    });
 
     cardPhotographer.classList.add("photographer-cards");
     cardImg.classList.add("cards-img");
@@ -94,8 +107,9 @@ function displayPhotographers() {
     cardLocation.classList.add("cards-location");
     cardTagline.classList.add("cards-tagline");
     cardPrice.classList.add("cards-price");
-    cardTags.classList.add("cards-tags");
     modalBg.classList.add("modal");
+
+    cardBody.classList.add("cards-body");
 
     cardLink.href = linkToPage;
     cardLink.setAttribute("role", "link");
@@ -104,18 +118,10 @@ function displayPhotographers() {
     cardLocation.textContent = photographer.city + ", " + photographer.country;
     cardTagline.textContent = photographer.tagline;
     cardPrice.textContent = photographer.price + "€/Jour";
-    tagsspan.textContent = photographer.tags;
 
     main.append(cardLink);
     cardLink.append(cardPhotographer);
-    cardPhotographer.append(
-      cardImg,
-      cardTitle,
-      cardLocation,
-      cardTagline,
-      cardPrice,
-      cardTags
-    );
-    cardTags.append(tagsA, tagsspan);
+    cardPhotographer.append(cardImg, cardTitle, cardBody);
+    cardBody.append(cardLocation, cardTagline, cardPrice, cardTags);
   });
 }
